92f4d74130c3b0b7bbb5a906c0b1dbf94723383e
Hyunseok Seo <hsseo0501@gmail.com>
2020-10-28 19:02:27 +0900

    Added country GDP page to show GDP

--- worldgdp-jhipster-react/worldgdp/src/main/webapp/app/countries/search/index.tsx
+++ worldgdp-jhipster-react/worldgdp/src/main/webapp/app/countries/search/index.tsx
@@ -4,10 +4,12 @@ import {Switch} from 'react-router-dom';
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
 import Country from './country';
+import CountryGdp from "./country-gdp";
 
 const Routes = ({match}) => (
   <>
     <Switch>
+      <ErrorBoundaryRoute exact path={`${match.url}/:id`} component={CountryGdp}/>
       <ErrorBoundaryRoute path={match.url} component={Country}/>
     </Switch>
   </>
