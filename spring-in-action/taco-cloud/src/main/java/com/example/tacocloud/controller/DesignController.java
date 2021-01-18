0a908b1ceb789e085615b3d544cd8e1828b6811b
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-18 22:54:59 +0900

    Add validation about model, controller and template for taco and order
      The model declares validation rules, the controller validates rule at form binding,
      and the template displays validation error.

--- spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/DesignController.java
+++ spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/DesignController.java
@@ -6,9 +6,13 @@ import com.example.tacocloud.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -40,6 +44,18 @@ public class DesignController {
         model.addAttribute("design", new Taco());
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
--- spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
+++ spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
@@ -4,9 +4,12 @@ import com.example.tacocloud.model.Order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -19,9 +22,12 @@ public class OrderController {
         return "order";
     }
 
-    // TODO: will be implemented later chapter
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "order";
+        }
+
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
