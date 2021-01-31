71ed2c299bf8fa3b72070e23d922bc201adc71ec
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-31 19:25:02 +0900

    Modify model classes in terms of trivial such as indent, etc.

--- spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/model/Taco.java
+++ spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/model/Taco.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -16,15 +17,15 @@ public class Taco {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private Date createdAt;
-
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
+    private Date createdAt;
+
     @ManyToMany(targetEntity = Ingredient.class)
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<Ingredient> ingredients = new ArrayList<>();
 
     @PrePersist
     void createdAt() {
