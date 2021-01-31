708d3bc5977d8eacfd6999d4b7f28da61e65d885
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-01 05:03:14 +0900

    Modify home controller test with JPA repository

--- spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/HomeControllerTest.java
+++ spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/HomeControllerTest.java
@@ -6,9 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.example.tacocloud.repository.IngredientRepository;
+import com.example.tacocloud.repository.OrderRepository;
+import com.example.tacocloud.repository.TacoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -16,6 +20,15 @@ public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private TacoRepository designRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     public void testHomePage() throws Exception {
