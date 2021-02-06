5bc2367da69ce03f6b10d31cf7bcfc81ee8dbb48
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-06 22:21:56 +0900

    Modify order model and controller to reuse user info

--- spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
+++ spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/controller/OrderController.java
@@ -1,16 +1,15 @@
 package com.example.tacocloud.controller;
 
 import com.example.tacocloud.model.Order;
+import com.example.tacocloud.model.security.User;
 import com.example.tacocloud.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
@@ -29,16 +28,32 @@ public class OrderController {
     }
 
     @GetMapping("/current")
-    public String showOrderForm(Model model) {
-        model.addAttribute("order", new Order());
+    public String showOrderForm(@AuthenticationPrincipal User user, @ModelAttribute Order order) {
+        if (order.getDeliveryName() == null) {
+            order.setDeliveryName(user.getFullname());
+        }
+        if (order.getDeliveryStreet() == null) {
+            order.setDeliveryStreet(user.getStreet());
+        }
+        if (order.getDeliveryCity() == null) {
+            order.setDeliveryCity(user.getCity());
+        }
+        if (order.getDeliveryState() == null) {
+            order.setDeliveryState(user.getState());
+        }
+        if (order.getDeliveryZip() == null) {
+            order.setDeliveryZip(user.getZip());
+        }
         return "order";
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus, @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "order";
         }
+
+        order.setUser(user);
 
         orderRepository.save(order);
         sessionStatus.setComplete();
