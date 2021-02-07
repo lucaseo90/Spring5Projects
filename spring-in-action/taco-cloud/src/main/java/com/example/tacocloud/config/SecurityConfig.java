9d5a6a42e6be780f5accde7faf2ed8882877c2d5
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-07 21:47:48 +0900

    Modify security config for UserDetailsService using field injection
      The constructor injection makes a cycle between SecurityConfig
        and UserDetailsServiceAutoConfiguration

--- spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/config/SecurityConfig.java
+++ spring-in-action/taco-cloud/src/main/java/com/example/tacocloud/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.tacocloud.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,12 +16,8 @@ import org.springframework.security.crypto.password.StandardPasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
     @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+    private UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder encoder() {
