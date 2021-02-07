03003a184e17ee1ccd13802c25bb14a6db304061
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-07 23:07:48 +0900

    Modify design and home controller unit test

--- spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/DesignControllerTest.java
+++ spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/DesignControllerTest.java
@@ -1,22 +1,32 @@
 package com.example.tacocloud.controller;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+import com.example.tacocloud.model.security.User;
 import com.example.tacocloud.repository.IngredientRepository;
 import com.example.tacocloud.repository.TacoRepository;
+import com.example.tacocloud.repository.security.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @WebMvcTest(DesignController.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class DesignControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
 
     @MockBean
     private IngredientRepository ingredientRepository;
@@ -24,7 +34,16 @@ public class DesignControllerTest {
     @MockBean
     private TacoRepository tacoRepository;
 
+    @BeforeAll
+    public void initTestUser() {
+        when(userRepository.findByUsername("user"))
+                .thenReturn(new User(
+                        "user", "pwd", "full name", "street", "city",
+                        "state", "zip", "phone number"));
+    }
+
     @Test
+    @WithMockUser(username = "user", password = "pwd") // This is even operated without username and password properties
     public void testShowDesignForm() throws Exception {
         mockMvc.perform(get("/design"))
                 .andExpect(status().isOk())
--- spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/HomeControllerTest.java
+++ spring-in-action/taco-cloud/src/test/java/com/example/tacocloud/controller/HomeControllerTest.java
@@ -1,19 +1,18 @@
 package com.example.tacocloud.controller;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
 import com.example.tacocloud.repository.IngredientRepository;
 import com.example.tacocloud.repository.OrderRepository;
 import com.example.tacocloud.repository.TacoRepository;
+import com.example.tacocloud.repository.security.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 public class HomeControllerTest {
@@ -22,10 +21,13 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
     private IngredientRepository ingredientRepository;
 
     @MockBean
-    private TacoRepository designRepository;
+    private TacoRepository tacoRepository;
 
     @MockBean
     private OrderRepository orderRepository;
