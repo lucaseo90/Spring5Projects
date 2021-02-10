93b4879ea70ff6564cff0fb1ec53098a4298310b
Hyunseok Seo <hsseo0501@gmail.com>
2021-02-10 16:17:52 +0900

    Create kong-client project

---
+++ kong-client/src/main/java/com/example/kongclient/KongClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.kongclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KongClientApplication.class, args);
+    }
+
+}
---
+++ kong-client/src/test/java/com/example/kongclient/KongClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kongclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
